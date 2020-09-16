d848b37234f66f2ebb9e4e8382c83327db075e56
Jonathan Canales <joncanales2@gmail.com>
2020-09-16 14:09:29 -0400

    updated script testing, trying to find values of forms & have them display in a console.log

--- canales-code/api-TARGETTING.js
+++ canales-code/api-TARGETTING.js
@@ -2,60 +2,74 @@ $(document).ready(function(){
     console.log("hello cruel world")
 
     
+    var generateNew = $("#generate-persona");
+    var targetContainer = $("#target-container");
+    var targetForm = $("#target-form");
+    var quoteSelect = $("#quote-type");
+    var personaSelect = $("#pesona-interests");
+    var submitGenerate = $("#submit-generate");
 
-    var target = $("#target-container")
+    targetForm.on("click", function() {
+      console.log(personaSelect.val())
+
+
+
+
+    })
+
+    // <div id="target-form"></div>
+
     // $("#persona-gen").on("click", function() {
     
-    $.ajax({
-        url: 'https://api.randomuser.me/',
-        dataType: 'json',
-        success: function(data) {
-          console.log(data);
-          console.log(data.results);
-          // display profile photo
-          var largeProfile = (data.results[0].picture.large)
-          // full name, could easily do without the "title"
-          var fullName = (data.results[0].name.title) + " " + (data.results[0].name.first) + " " + (data.results[0].name.last);
+    // $.ajax({
+    //     url: 'https://api.randomuser.me/',
+    //     dataType: 'json',
+    //     success: function(data) {
+    //       console.log(data);
+    //       console.log(data.results);
+    //       // display profile photo
+    //       var largeProfile = (data.results[0].picture.large)
+    //       // full name, could easily do without the "title"
+    //       var fullName = (data.results[0].name.title) + " " + (data.results[0].name.first) + " " + (data.results[0].name.last);
           
-          var gender =(data.results[0].gender);
-          var age = (data.results[0].dob.age);
-          var city = (data.results[0].location.city);
-          var state = (data.results[0].location.state);
-          var nationality = (data.results[0].nat);
+    //       var gender =(data.results[0].gender);
+    //       var age = (data.results[0].dob.age);
+    //       var city = (data.results[0].location.city);
+    //       var state = (data.results[0].location.state);
+    //       var nationality = (data.results[0].nat);
          
-          var userName = (data.results[0].login.username);
+    //       var userName = (data.results[0].login.username);
           
           
-          var personaDiv = $("<div>");
-          target.append(personaDiv);
+    //       var personaDiv = $("<div>");
+    //       target.append(personaDiv);
           
-          var personaImg = $("<img>");
-          personaImg.attr("src", largeProfile);
-          personaDiv.append(personaImg);
+    //       var personaImg = $("<img>");
+    //       personaImg.attr("src", largeProfile);
+    //       personaDiv.append(personaImg);
 
-          var personaName = $("<h1>");
-          personaName.addClass("testPersonaFont");
-          personaName.text("NAME: " + fullName);
-          personaDiv.append(personaName);
+    //       var personaName = $("<h1>");
+    //       personaName.addClass("testPersonaFont");
+    //       personaName.text("NAME: " + fullName);
+    //       personaDiv.append(personaName);
 
-          var personaDemographic = $("<h1>");
-          personaDemographic.addClass("testPersonaFont");
-          personaDemographic.text("SEX: " + gender + "   " + "AGE: " + age);
-          personaDiv.append(personaDemographic);
+    //       var personaDemographic = $("<h1>");
+    //       personaDemographic.addClass("testPersonaFont");
+    //       personaDemographic.text("SEX: " + gender + "   " + "AGE: " + age);
+    //       personaDiv.append(personaDemographic);
 
-          var personaGeoGraphic = $("<h1>");
-          personaGeoGraphic.addClass("testPersonaFont");
-          personaGeoGraphic.text("City: " + city + "  " + "State: " + state);
-          personaDiv.append(personaGeoGraphic)
+    //       var personaGeoGraphic = $("<h1>");
+    //       personaGeoGraphic.addClass("testPersonaFont");
+    //       personaGeoGraphic.text("City: " + city + "  " + "State: " + state);
+    //       personaDiv.append(personaGeoGraphic)
 
 
           
 
 
 
-        }
-      });
-    // });
+    //     }
+    //   });
 
 
 
