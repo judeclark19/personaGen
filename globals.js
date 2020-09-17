9bc2179c509a8173fcf94f0c89f1f4ac8a7f050c
Joseph Perry <josephperry720@gmail.com>
2020-09-17 14:33:04 -0400

    created interests/career array

--- globals.js
+++ globals.js
@@ -1,4 +1,4 @@
-signs = [
+astroSigns = [
   "Aries",
   "Taurus",
   "Gemini",
@@ -13,4 +13,45 @@ signs = [
   "Pisces",
 ];
 
-interests = [];
+function generateSign() {
+    var randomMonth = Math.floor(Math.random * astroSigns.length);
+    return astroSigns[randomMonth];
+}
+
+interests = [
+    {title:"finance", ageRange: [20, 70]},
+    {title:"acting", ageRange: [18, 40]},
+    {title:"architecture", ageRange: [40, 80]},
+    {title:"art", ageRange: [15, 70]},
+    {title:"baking", ageRange: [20, 50]},
+    {title:"ballet", ageRange: [16, 30]},
+    {title:"bartending", ageRange: [21, 50]},
+    {title:"biology", ageRange: [30, 70]},
+    {title:"finance", ageRange: [20, 70]},
+    {title:"photography", ageRange: [18, 40]},
+    {title:"film", ageRange: [20, 40]},
+    {title:"caretaking", ageRange: [35, 55]},
+    {title:"cooking", ageRange: [16, 40]},
+    {title:"computer programming", ageRange: [25, 60]},
+    {title:"dentistry", ageRange: [40, 70]},
+    {title:"doctor", ageRange: [40, 70]},
+    {title:"engineering", ageRange: [30, 70]},
+    {title:"farming", ageRange: [30, 70]},
+    {title:"floristry", ageRange: [50, 90]},
+    {title:"gardening", ageRange: [50, 90]},
+    {title:"rock music", ageRange: [18, 35]},
+    {title:"law", ageRange: [25, 80]},
+    {title:"modeling", ageRange: [18, 30]},
+    {title:"nursing", ageRange: [30, 70]},
+    {title:"pharmaceuticals", ageRange: [35, 70]},
+    {title:"politics", ageRange: [27, 70]},
+    {title:"real estate", ageRange: [40, 70]},
+    {title:"the church", ageRange: [40, 100]},
+    {title:"science", ageRange: [40, 70]},
+    {title:"singing", ageRange: [10, 30]},
+    {title:"academia", ageRange: [35, 70]},
+    {title:"teaching", ageRange: [25, 70]},
+    {title:"vetenary care", ageRange: [25, 70]},
+    {title:"classical music", ageRange: [35, 80]},
+    {title:"restaurants", ageRange: [15, 50]},
+];
